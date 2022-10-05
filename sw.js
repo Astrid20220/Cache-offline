1cf813627d018cb41fee1bb698da27a1833d8ca9
Astrid20220 <yadiragonzalez556@gmail.com>
2022-10-05 13:36:16 -0500

    Respuesta offline basica

--- sw.js
+++ sw.js
@@ -1,3 +1,18 @@
 
 
+self.addEventListener('fetch', event => {
+    const offlineResp = new Response(`
+    
+    Bienvenido a mi pagina web
+
+    Disculpa, pero para usarla, nevcesitas internet
+    `);
+
+
+    const resp = fetch(event.request)
+                     .catch( () => offlineResp);
+
+    event.respondWith(resp);
+});
+
 

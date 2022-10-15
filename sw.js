df32e33d2507f677abe4b79c323d82d551c3b285
Astrid20220 <yadiragonzalez556@gmail.com>
2022-10-14 22:37:16 -0500

    Cache only

--- sw.js
+++ sw.js
@@ -1,52 +1,28 @@
 
+self.addEventListener('install', e => {
+   const cacheProm = caches.open('cache-1').then(cache => {
+       cache.addAll([
+           '/',
+           '/index.html',
+           '/css/style.css',
+           '/img/main.jpg',
+self.addEventListener('install', e => {
+   });
 
-self.addEventListener('fetch', event => {
-    //const offlineResp = new Response(`
-    
-    //Bienvenido a mi pagina web
-
-   // Disculpa, pero para usarla, nevcesitas internet
-    //`);
-
-//const offlineResp = new Response(`
-   // <!DOCTYPE html>
-//<html lang="en">
-//<head>
-   // <meta charset="UTF-8">
-    //<meta name="viewport" content="width=device-width, initial-scale=1.0">
-    //<meta http-equiv="X-UA-Compatible" content="ie=edge">
-    //<title>Mi PWA</title>
-
-    //</head>
-    //<body class="container p-3>
-
-    //<h1>Offline Mode</h1>
-
-    //</body>
-   // </html>
-
-//</head>
-//<body class="container p-3">
-    
-    
-    // `, {
-       // headers:{
-            //'Content-Type':'text/html'
-       // }
-     //});
+   e.waitUntil(cacheProm);
+});
 
 
-     self.addEventListener('install', e => {
-      const cacheProm = caches.open('cache-1').then(cache => {
-          cache.addAll([
-              '/index.html',
-              '/css/style.css',
-              '/img/main.jpg',
-              'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css',
-              '/js/app.js'
-          ]);
-      });
-  
-      e.waitUntil(cacheProm);
-  });
 
+
+self.addEventListener('fetch', e => {
+
+
+
+
+
+
+   // 1 - Cache Only
+   e.respondWith(caches.match(e.request));
+
+});

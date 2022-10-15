afa12bb91125187bf22b3b7a12d9b29a7f55ed3e
Astrid20220 <yadiragonzalez556@gmail.com>
2022-10-14 22:48:46 -0500

    network with cache fallback

--- sw.js
+++ sw.js
@@ -1,45 +1,70 @@
-
-const CACHE_NAME = 'cache-1';
-
-// const CACHE_NAME = 'cache-1';
-const CACHE_STATIC_NAME = 'static-v1';
+const CACHE_STATIC_NAME = 'static-v2';
 const CACHE_DYNAMIC_NAME = 'dynamic-v1';
 const CACHE_INMUTABLE_NAME = 'inmutable-v1';
+const CACHE_DYNAMIC_LIMIT = 50;
 
 
 
-
-self.addEventListener('install', e => {
-   const cacheProm = caches.open('cache-1').then(cache => {
-      const cacheProm = caches.open('CACHE_NAME').then(cache => {
-       cache.addAll([
-
-           '/',
-           '/index.html',
-          
-self.addEventListener('install', e => {
-   });
-
-   e.waitUntil(cacheProm);
-   const cacheInmutable = caches.open(CACHE_INMUTABLE_NAME).then(cache => cache.add('https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css'));
-
-   e.waitUntil(Promise.all([cacheProm, cacheInmutable]));
-});
+@@ -47,29 +48,49 @@ self.addEventListener('fetch', e => {
 
 
 
+    // 2 - Cache With Network Fallback
+    const respuesta = caches.match(e.request).then(res => {
+        if (res) return res;
 
-e.respondWith(respuesta);
+        // No existe el archivo 
+        // tengo que ir a la web
+        console.log('No existe', e.request.url);
+    // 3 - Network with cache fallback
+
+        return fetch(e.request)
+            .then(newResp => {
+
+                caches.open(CACHE_DYNAMIC_NAME).
+                then(cache => {
+                    cache.put(e.request, newResp);
+                    limpiarCache(CACHE_DYNAMIC_NAME, 5);
+                });
+                return newResp.clone();
+    const respuesta = fetch(e.request).then(res => {
+        if (!res) return caches.match(e.request);
+        console.log('Fetch', res);
+        caches.open(CACHE_DYNAMIC_NAME)
+            .then(cache => {
+                cache.put(e.request, res);
+                limpiarCache(CACHE_DYNAMIC_NAME, CACHE_DYNAMIC_LIMIT);
+            });
+
+        return res.clone();
+    }).catch(err => {
+        return caches.match(e.request);
+    });
+
+    e.respondWith(respuesta);
 
 
-self.addEventListener('fetch', e => {
+    e.respondWith(respuesta);
+    // 2 - Cache With Network Fallback
+    // const respuesta = caches.match(e.request).then(res => {
+    //     if (res) return res;
+
+    //     // No existe el archivo 
+    //     // tengo que ir a la web
+    //     console.log('No existe', e.request.url);
+
+    //     return fetch(e.request)
+    //         .then(newResp => {
+
+    //             caches.open(CACHE_DYNAMIC_NAME).
+    //             then(cache => {
+    //                 cache.put(e.request, newResp);
+    //                 limpiarCache(CACHE_DYNAMIC_NAME, 5);
+    //             });
+    //             return newResp.clone();
+    //         });
+    // });
 
 
 
-
-
-
-   // 1 - Cache Only
-   e.respondWith(caches.match(e.request));
-
-});
+    // e.respondWith(respuesta);

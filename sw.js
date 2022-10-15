25c77983f01efd86ca3c0461a60eadbccbfa5e6c
Astrid20220 <yadiragonzalez556@gmail.com>
2022-10-14 22:43:29 -0500

    Cache dinamico

--- sw.js
+++ sw.js
@@ -1,4 +1,14 @@
 
+const CACHE_NAME = 'cache-1';
+
+// const CACHE_NAME = 'cache-1';
+const CACHE_STATIC_NAME = 'static-v1';
+const CACHE_DYNAMIC_NAME = 'dynamic-v1';
+const CACHE_INMUTABLE_NAME = 'inmutable-v1';
+
+
+
+
 self.addEventListener('install', e => {
    const cacheProm = caches.open('cache-1').then(cache => {
       const cacheProm = caches.open('CACHE_NAME').then(cache => {
@@ -11,26 +21,11 @@ self.addEventListener('install', e => {
    });
 
    e.waitUntil(cacheProm);
+   const cacheInmutable = caches.open(CACHE_INMUTABLE_NAME).then(cache => cache.add('https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css'));
+
+   e.waitUntil(Promise.all([cacheProm, cacheInmutable]));
 });
 
- // 2 - Cache With Network Fallback
- const respuesta = caches.match(e.request).then(res => {
-   if (res) return res;
-
-   // No existe el archivo 
-   // tengo que ir a la web
-   console.log('No existe', e.request.url);
-
-   return fetch(e.request)
-       .then(newResp => {
-
-           caches.open(CACHE_NAME).
-           then(cache => {
-               cache.put(e.request, newResp);
-           });
-           return newResp.clone();
-       });
-});
 
 
 
